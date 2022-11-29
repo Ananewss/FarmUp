1849daed2ee55d7fc75ba9e9f68e9d092cc35bfb
LAPTOP-MP7C0896\MontreeK <zmontreek@pttdigital.com>
2022-11-29 19:06:58 +0700

    Change Auto redirect to register page if user line id is not exist

--- FarmUp/FarmUp/Controllers/BlankController.cs
+++ FarmUp/FarmUp/Controllers/BlankController.cs
@@ -1,12 +1,25 @@
 ﻿using FarmUp.Dtos.Admin;
+using FarmUp.Services.Admin;
+using FruitProject.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 using Newtonsoft.Json;
 using System.Net;
+using static Org.BouncyCastle.Math.EC.ECCurve;
 
 namespace FarmUp.Controllers
 {
     public class BlankController : Controller
     {
+
+        private IConfiguration _config;
+        private readonly ILogger<BlankController> _logger;
+        public BlankController(IConfiguration config, ILogger<BlankController> logger)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -22,6 +35,35 @@ namespace FarmUp.Controllers
                 var refer = (dict.ContainsKey("ref"))? dict["ref"] : "";
 
                 HttpContext.Session.SetString("lineUserId", lineUserId);
+
+
+                string strConn = _config.GetConnectionString($"onedurian");
+
+                MySqlConnection mSqlConn = new MySqlConnection(strConn);
+                try
+                {
+                    var sql = @"SELECT * 
+                        FROM ma_user
+                        WHERE usr_line_id = @usr_line_id";
+                    mSqlConn.Open();
+
+                    MySqlCommand mCmd = new MySqlCommand(sql, mSqlConn);
+                    mCmd.Parameters.AddWithValue("@usr_line_id", lineUserId);
+
+                    var readData = mCmd.ExecuteReader();
+                    if (!readData.Read())
+                    {
+                        refer = "Account-Register";
+                    }
+                    readData.Close();
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+                mSqlConn.Close();
+
                 //HttpContext.Session.GetString("lineUserId")
 
                 //if (refer != null)
